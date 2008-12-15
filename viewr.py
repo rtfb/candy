rtfb/candy

commit 7a50118cea8d22b93bc97ec88d68c9d0178035f4

Fixed a bug with Viewer refusing to start
The problem was wrong argument passed as a parent. This happened in the
process of series of refactorings introducing the MVC pattern.

The changes in viewr.py are cosmetic only.

viewr.py
@@ -31,7 +31,8 @@ class BuiltinViewerControl (stc.StyledTextCtrl):
         self.Bind (wx.EVT_KEY_DOWN, self.OnKeyDown)
         self.Bind (wx.EVT_WINDOW_DESTROY, self.OnDestroy)
 
-        self.StyleSetSpec (stc.STC_STYLE_DEFAULT, "size:%d,face:%s" % (pb, faceCourier))
+        styleSpec = 'size:%d,face:%s' % (pb, faceCourier))
+        self.StyleSetSpec (stc.STC_STYLE_DEFAULT, styleSpec)
         self.SetFocus ()
 
     def OnDestroy (self, evt):

candy.py
@@ -665,7 +665,7 @@ class PanelController (object):
     def startViewer (self):
         import viewr
         file = self.model.items[self.selectedItem].fileName
-        wnd = viewr.BuiltinViewerFrame (self, -1, file, file)
+        wnd = viewr.BuiltinViewerFrame (self.view, -1, file, file)
         wnd.Show (True)
 
     def switchPane (self):

commit 3fbed8b46ca4af823d829bd4c5b650dc7dfcd0f1

Fixed a breaking typo in Viewer
Introduced with 7a50118cea8d22b93bc97ec88d68c9d0178035f4.

viewr.py
@@ -31,7 +31,7 @@ class BuiltinViewerControl (stc.StyledTextCtrl):
         self.Bind (wx.EVT_KEY_DOWN, self.OnKeyDown)
         self.Bind (wx.EVT_WINDOW_DESTROY, self.OnDestroy)
 
-        styleSpec = 'size:%d,face:%s' % (pb, faceCourier))
+        styleSpec = 'size:%d,face:%s' % (pb, faceCourier)
         self.StyleSetSpec (stc.STC_STYLE_DEFAULT, styleSpec)
         self.SetFocus ()
 